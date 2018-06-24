aa218d8862b25f3a477a959f18ad42b7e6fe3939
thalvari <tuomas.halvari@helsinki.fi>
2018-06-24 04:01:11 +0300

    Korjattu bugi

--- application/models.py
+++ application/models.py
@@ -6,9 +6,9 @@ from application import db
 class Base(db.Model):
     __abstract__ = True
     id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp(),
-                              nullable=False)
+    date_created = db.Column(db.DateTime, server_default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, server_default=db.func.current_timestamp(),
+                              server_onupdate=db.func.current_timestamp())
 
     def get_date_created_no_millis(self):
         return self.date_created.strftime('%Y-%m-%d %H:%M:%S')
